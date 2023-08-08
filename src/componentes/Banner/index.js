d5569216b46bd717cf67663fbfbc482efbee56e8
Lunna Camargo <lunna@pumpycode.com>
2023-08-07 23:22:51 -0300

    inicio organo react

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal da pagina" />
+        </header>
+    )
+
+}
+
+export default Banner
